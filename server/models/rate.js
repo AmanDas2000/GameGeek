28a3dce80166b20dc213eb1fe66d4865ab3235b9
FxGs <avimcw@protonmail.ch>
2021-03-08 23:41:03 +0530

    added list model and route, added message in json response for toast

--- server/models/rate.js
+++ server/models/rate.js
@@ -1,18 +1,22 @@
 const mongoose = require('mongoose')
-const {ObjectId} = mongoose.Schema.Types
+const {
+    ObjectId
+} = mongoose.Schema.Types
 const postSchema = new mongoose.Schema({
-    game:{
-        type:ObjectId,
-        ref:"Game"
+    game: {
+        type: ObjectId,
+        ref: "Game"
     },
-    rating:{
-        type:Number,
-        required:true
+    rating: {
+        type: Number,
+        default: 0,
+        min: 0.1,
+        max: 10
     },
-    postedBy:{
-       type:ObjectId,
-       ref:"User"
+    postedBy: {
+        type: ObjectId,
+        ref: "User"
     }
 })
 
-mongoose.model("Rate",postSchema)
+mongoose.model("Rate", postSchema)
